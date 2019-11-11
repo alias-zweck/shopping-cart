9f70b0642d8891dabbe837b41c9c4c7bd0cb01f8
Basil k <basil.k@zweck.io>
2019-11-11 18:33:44 +0530

    product and cart template structure using bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CartComponent } from './components/cart/cart.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { ProductComponent } from './components/products/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    CartComponent,
+    SummaryComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/products/product/product.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent {
+
+  cart()
+  {
+    
+  }
+}
